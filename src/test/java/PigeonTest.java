c5e5f14ec7adb2c0c2e47b630fca0b13adc6003c
grace-fengqing-yu <fqgrace.hi@gmail.com>
2021-09-27 19:58:53 -0400

    Add class Pigeon that implements tradable and domesticatable interface. Modified Trader main method.

---
+++ src/test/java/PigeonTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class PigeonTest {
+    Pigeon p;
+
+    @Before
+    public void setUp() throws Exception {
+        p = new Pigeon(0.5);
+    }
+
+    @Test(timeout = 50)
+    public void TestSound() {
+        assertEquals("woo", p.sound());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(2, p.getPrice());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetWeight() {
+        assertEquals(0.5, p.getWeight());
+    }
+}
